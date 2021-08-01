ffca701f818c6b2924d92511bd32eea834480cf2
Ayomide <ayomideawonaya@gmail.com>
2021-08-01 10:37:07 -0400

    edited

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -2,9 +2,9 @@ const {format_date} = require('../utils/helpers');
 
 // creating test to that format_date() takes Date() objects and returns dates in MM/DD/YYYY
 test('format_date() returns a date string', () => {
-    const date = new Date('2020-03-20 16:12:03');
+    const date = new Date('2021-07-1 16:12:03');
   
-    expect(format_date(date)).toBe('3/20/2020');
+    expect(format_date(date)).toBe('1/07/2021');
   });
 
 const {format_plural} = require('../utils/helpers')
