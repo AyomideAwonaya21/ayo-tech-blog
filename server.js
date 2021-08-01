52c3438aee7a08ce6bdb6a1eebe96228ce118107
Ayomide <ayomideawonaya@gmail.com>
2021-08-01 11:45:21 -0400

    made changes on server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: 'Super secret secret',
-    cookie: { maxAge: 36000 },
+    cookie: { },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
@@ -21,10 +21,11 @@ const sess = {
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.use(routes);
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-});
+//app.use(express.static(path.join(__dirname, 'public')));
+//app.engine('handlebars', hbs.engine);
+//app.set('view engine', 'handlebars');
+//app.use(routes);
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+    sequelize.sync({ force: false });
+  });
