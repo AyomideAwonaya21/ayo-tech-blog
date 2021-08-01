8b608657f9b61a0e52a833f2f79554ef219766e6
Ayomide <ayomideawonaya@gmail.com>
2021-08-01 12:37:39 -0400

    made some chnages to handlebars

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ const sess = {
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.static(path.join(__dirname, 'public')));
-//app.engine('handlebars', hbs.engine);
-//app.set('view engine', 'handlebars');
-//app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(routes);
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
     sequelize.sync({ force: false });
